7b80d6b02ded7ae485b4867b5d58c736e55cad5b
Nguyen Son <sonnguyen@tamdongtam.vn>
2019-09-27 11:20:27 +0700

    update

--- recom_point/student/models.py
+++ recom_point/student/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from .utils import convert_diem_so_thanh_diem_chu
 # Create your models here.
+
+def convert_diem_so_thanh_diem_chu(diem):
+    if diem >= 8.5:
+        return 'A'
+    if diem >= 8.0:
+        return 'B+'
+    if diem >= 7.0:
+        return 'B'
+    if diem >= 6.5:
+        return 'C+'
+    if diem >= 5.5:
+        return 'C'
+    if diem >= 5.0:
+        return 'D'
+    if diem < 5:
+        return 'F'
 
 
 class Khoa(models.Model):
